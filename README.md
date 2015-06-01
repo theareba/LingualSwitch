47d1a1fe7243558edea8832faafdbfac5886272b
Victor Areba <vicgoburg@gmail.com>
2015-06-02 01:37:09 +0300

    Summary commit

---
+++ README.md
@@ -0,0 +1,8 @@
+== README
+
+Simple app that demonstrates how to integrate switching menu is a multiligual site
+with each site having it's own domain.
+
+Note: For production, you'll need to add those domain in your DNS settings.
+
+You can access the tutorial by clicking here [here](http://www.victorareba.com/tutorials/multilingual-application-with-domain-switch-in-rails)
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+  end
+
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+end
